73f1ca8a6d3306de01b8af4f119e86aab5916149
Hanumavathi <hanumavathipothuganti06@gmail.com>
2025-01-07 18:53:10 +0530

    loops,methods

---
+++ src/com/mru/methods/Methods.java
@@ -0,0 +1,20 @@
+package com.mru.methods;
+
+public class Methods {
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Methods obj = new Methods();
+		MRUniv obj = new MRUniv();
+		MRUniv.dsa1();
+		MRUniv.dsa2("MRU");
+		String res = MRUniv.dsa3();
+		System.out.println(res);
+		String res1 = MRUniv.dsa4("Codechef");
+		System.out.println(res1);
+
+	}
+
+}
